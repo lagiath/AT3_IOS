605f92d93cb85344c5df4587385cb5601397fa2e
Daniel Gabb <gabb.dan7@gmail.com>
2024-05-06 14:50:08 +1000

    Initial Commit

---
+++ AT3_IOS/AT3_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AT3_IOSApp.swift
+//  AT3_IOS
+//
+//  Created by Daniel Gabb on 6/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct AT3_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AT3_IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AT3_IOS
+//
+//  Created by Daniel Gabb on 6/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
